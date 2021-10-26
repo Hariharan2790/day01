37d1e1c3b494054842e7c1fc41a1a7f53597bcf2
hariharan <hariharan.mct@gmail.com>
2021-10-26 15:57:34 +0530

    First commit of the day 01

---
+++ script.js
@@ -0,0 +1,25 @@
+//XML  HTTP REQUEST(xhr) (VIA API)
+// Step 01: create a XHR object
+var  request= new XMLHttpRequest();
+//Step 02:initatiate a connection(starting a connections)
+//true is used if your api doesnot want to affect the remaining  execution of your code
+
+request.open('GET','https://restcountries.com/v2/all',true);
+//step 03: sending a connection
+
+request.send();
+//step 04: once the data has been received successfully from server we need to extract te data  from JSON 
+
+//some conversion is needed here 
+request.onload=function(){
+    var data=JSON.parse(request.response);
+    //console.log(data);
+    let res=data.filter((ele)=>ele.population<100000);
+    console.log(res);
+
+    
+    //for(var i=0; i<data.length;i++){
+      //  console.log(data[i].name.official +" - "+ data[i].region);
+    
+    
+}
